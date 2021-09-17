b4dcf131523a75b56a56b254a9dd3bb64ba6e429
hillaliy <hillaliy@me.com>
2021-09-17 12:29:53 +0300

    Update settings.js

--- dist/settings.js
+++ dist/settings.js
@@ -1,12 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PLUGIN_NAME = exports.PLATFORM_NAME = void 0;
-/**
- * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
- */
+// This is the name of the platform that users will use to register the plugin in the Homebridge config.json
 exports.PLATFORM_NAME = 'haa-downloader';
-/**
- * This must match the name of your plugin as defined the package.json
- */
+// This must match the name of your plugin as defined the package.json
 exports.PLUGIN_NAME = 'homebridge-haa-downloader';
 //# sourceMappingURL=settings.js.map
